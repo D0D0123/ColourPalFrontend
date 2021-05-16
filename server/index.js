48e5a1662e8c1f439977085ada59f86ce4d819ab
Soura <z5311640@ad.unsw.edu.au>
2021-05-16 23:09:27 +1000

    now it finally sends the colour info, file.mv had to be awaited

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const ColorThief = require('colorthief');
 const {resolve} = require('path');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const multer = require('multer');
+const vision = require('@google-cloud/vision');
+
 
 //middleware
+const client = new vision.ImageAnnotatorClient();
 app.use(cors());
 app.use(fileUpload());
 
@@ -15,51 +17,24 @@ app.listen(8000, () => {
     console.log("Server is starting on port 8000");
 });
 
-// function returnPalette(img) {
-//     var palette = ColorThief.getPalette(img, 10)
-//         .then(palette => { console.log(palette) })
-//         .catch(err => { console.log(err) })
-//     return palette;
-// }
-
-
-
 // ROUTES //
 app.post('/upload', async (req, res) => {
 
-    var img;
-    var file;
+    var file = req.files.file;
+    await file.mv(`${__dirname}/uploads/${file.name}`);
+    var filePath = `${__dirname}/uploads/${file.name}`;
+    console.log(filePath);
 
-    async function getFiles(req) {
-        file = req.files.file;
-        file.mv(`${__dirname}/uploads/${file.name}`);
+    try {
+        // Performs property detection on the local file
+        const [result] = await client.imageProperties(filePath);
+        console.log(result);
+        const colors = result.imagePropertiesAnnotation.dominantColors.colors;
+        colors.forEach(color => console.log(color));
+    } catch (err) {
+        console.error(err);
     }
 
-    async function getImg(file) {
-        // img = resolve(process.cwd(), `${__dirname}/uploads/${file.name}`);
-        img = `${__dirname}/uploads/${file.name}`;
-    }
-
-    await getFiles(req);
-    console.log(file.name);
-    await getImg(file);
-    console.log(img);
-    console.log("debug1");
-    
-    // ColorThief.getColor(img)
-    //     .then(color => { console.log(color) })
-    //     .catch(err => { console.log(err) })
-
-    // https://javascript.info/promise-basics
-
-    var palette;
-    await ColorThief.getPalette(img, 10)
-        .then(palette => { 
-            console.log(palette);
-            res.json({fileName: file.name, paletteObj: palette}); 
-        })
-        .catch(err => { console.log(err) })
-    
 });
 
 
